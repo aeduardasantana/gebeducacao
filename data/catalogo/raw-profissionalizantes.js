8b7d7fab603ecd5da8e778844d590e2501165d6a
Dudow Santana <85846782+aeduardasantana@users.noreply.github.com>
2026-09-21 12:20:58 -0300

    Adiciona base de cursos profissionalizantes GEB Educação

---
+++ data/catalogo/raw-profissionalizantes.js
@@ -0,0 +1,2 @@
+window.GEB_RAW=window.GEB_RAW||{};
+window.GEB_RAW['GEB Educação']=[["3D Studio Max Básico","Profissionalizante",120,"21 aulas"],["Access 2016","Profissionalizante",180,"35 aulas"],["Acompanhante Terapêutico","Profissionalizante",180,"14 aulas"],["Adestramento de Cães do Caos à Obediência","Profissionalizante",130,"24 aulas"],["Administração de hotéis","Profissionalizante",95,"23 aulas"]];
